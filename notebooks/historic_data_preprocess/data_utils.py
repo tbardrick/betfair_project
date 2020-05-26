2a3ab576f2ad4620b4b708a4718f5ffafa7cab8c
tb15g15 <41759285+tb15g15@users.noreply.github.com>
2020-05-26 22:58:10 +0100

    big daddy dataframe

---
+++ notebooks/historic_data_preprocess/data_utils.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+
+
+datadict = {'Time': [],
+       'MarketId' : [],
+       'Status' : [],
+       'Inplay' : [], 
+       'SelectionId' : [],
+       'LastPriceTraded' : [],
+       'TotalMatched' : [],
+       'BSP' : [],
+       'AdjFactor' :  [],
+       'RunnerStatus' : [],
+       'MktTotalMatched' : [],
+       'RaceInfo' : [],
+       'Venue' : [],
+       'BackSize': [],
+       'BackPrice': [],
+       'LayPrice' : [],
+       'LaySize' : []
+}
