dcbc6632af83fedfb44d2e984403dcba19c35926
tb15g15 <41759285+tb15g15@users.noreply.github.com>
2020-06-16 23:02:06 +0100

    strategy algo

--- notebooks/historic_data_strategies/strategies/utils.py
+++ notebooks/historic_data_strategies/strategies/utils.py
@@ -1,5 +1,8 @@
-def lay_hedge_max_lp(bp, c):
-	return (bp - 1) * (1 - c) ** 2 + 1
+def lay_hedge_threshold(bp, c):
+	return round((bp - 1) * (1 - c) + 1, 2)
 
-def back_hedge_min_bp(lp, c):
+def back_hedge_threshold(lp, c):
 	return (c ** 2 - 2 * c + lp) / (1 - c) ** 2
+
+def lay_hedge_stake(bp, bs, lp, c):
+    return round((((bp - 1) * bs * (1 - c)) + bs) / (lp - c), 2)
